b68aab800eb441ef7eae3d17c230a0d94592f36a
Ruslan <rv.volovik88@gmail.com>
2020-12-28 21:57:56 +0200

    new: intial commit mysql

--- templates/initial.sql
+++ templates/initial.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE IF NOT EXISTS shop;
--- DROP DATABASE IF EXISTS shop;
+DROP DATABASE IF EXISTS shop;
 
 CREATE TABLE IF NOT EXISTS users(
     id INT
 );
--- DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE IF NOT EXISTS people(
     id    INT NOT NULL AUTO_INCREMENT,
@@ -15,8 +15,8 @@ CREATE TABLE IF NOT EXISTS people(
     PRIMARY KEY(id)
 );
 
--- ALTER TABLE people ADD COLUMN IF NOT EXISTS password VARCHAR(32);
--- ALTER TABLE users DROP COLUMN IF EXISTS password;
+ALTER TABLE people ADD COLUMN IF NOT EXISTS password VARCHAR(32);
+ALTER TABLE users DROP COLUMN IF EXISTS password;
 
 INSERT INTO people(name, bio, birth, email) VALUES('Jake', 'Bio here', '2004-05-06', 'jake@jake.com');
 INSERT INTO people(name, email, birth) VALUES('Bob', 'bob@bob.com', '2003-05-09');
@@ -77,3 +77,29 @@ CREATE TABLE IF NOT EXISTS orders(
     FOREIGN KEY(orders.shop_id)   REFERENCES shop(id),
     FOREIGN KEY(orders.person_id) REFERENCES people(id)
 );
+
+INSERT INTO shop(title, price) VALUES
+    ('phone', 1000),
+    ('tv', 3000),
+    ('fridge', 2000);
+
+INSERT INTO orders(order_number, shop_id, person_id) VALUES
+    (0001, 2, 4),
+    (0002, 1, 1),
+    (0003, 3, 1);
+
+SELECT shop.title, people.name, people.email, orders.order_number FROM people
+    INNER JOIN orders ON people.id = orders.person_id
+    INNER JOIN shop ON shop.id = orders.shop_id
+    ORDER BY orders.order_number DESC;
+
+SELECT people.name, people.email, orders.order_number FROM people
+    LEFT JOIN orders ON people.id = orders.person_id
+    ORDER BY people.id DESC;
+
+SELECT people.name AS n, people.email AS e FROM people as p;
+SELECT CONCAT('name: ', people.name AS n, ', date of birth: ', people.birth AS b) AS information FROM people;
+SELECT * AS a, p.id AS i, s.title AS t FROM people AS p, shop AS s;
+SELECT COUNT(people.id) AS count, MIN(shop.price) AS min, AVG(shop.price) AS avg, SUM(people.id) AS sum FROM people, shop;
+SELECT UCASE(shop.title), LCASE(shop.title) FROM shop;
+SELECT price, COUNT(price) AS count FROM shop GROUP BY price HAVING COUNT(price) > 1;
